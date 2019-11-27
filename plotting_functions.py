2da51bc3b2da57e72e3677d867d573bedb168d65
mysh <mysh995@gmail.com>
2019-11-27 14:40:44 +0000

    Refactoring - generalised for plot type

--- plotting_functions.py
+++ plotting_functions.py
@@ -4,12 +4,46 @@ import pandas as pd
 import aux_functions as aux
 
 
-def pie_plot(df: pd.DataFrame, label, ax=None, show=True, **kwargs):
+def make_plot(df: pd.DataFrame, labels, nrows=2, show=True, title=None, fig_kwargs=None, **kwargs):
+    fig_kwargs = fig_kwargs or {}
+
+    ncols = len(labels) // nrows + int(bool(len(labels) % nrows))
+
+    fig, axes = plt.subplots(nrows, ncols, **fig_kwargs)
+
+    for i, label in enumerate(labels):
+        make_subplot(df, label, ax=axes[i//nrows, i % nrows], show=False, **kwargs)
+
+    plt.suptitle(title or "", fontsize=16)
+
+    if show:
+        plt.show()
+
+    return fig
+
+
+def count(df: pd.DataFrame, label):
+    return df[label].value_counts()
+
+
+def make_subplot(df, label, type='pie', ax=None, show=True, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
 
-    counts = df[label].value_counts()
+    counts = count(df, label)
 
+    if type == 'pie':
+        pie_subplot(counts, ax, **kwargs)
+    else:
+        raise ValueError(f"Plot type '{type}' not recognised")
+
+    if show:
+        plt.show()
+
+    ax.set_title(aux.break_label(counts.name, ['a ', 'do']))
+
+
+def pie_subplot(counts, ax, **kwargs):
     pie = ax.pie(counts, **kwargs)
 
     # Shrink current axis by 20%
@@ -19,23 +53,4 @@ def pie_plot(df: pd.DataFrame, label, ax=None, show=True, **kwargs):
     ax.legend(pie[0], aux.break_labels(counts.keys()), fontsize=10,
               bbox_transform=ax.transAxes, bbox_to_anchor=(1, 0.5), loc="center left")
 
-    ax.set_title(aux.break_label(counts.name, ['a ', 'do']))
 
-    if show:
-        plt.show()
-
-
-def pie_subplots(df: pd.DataFrame, labels, nrows=2, show=True, title=None, fig_kwargs=None):
-    fig_kwargs = fig_kwargs or {}
-
-    ncols = len(labels) // nrows + int(bool(len(labels) % nrows))
-    fig, axes = plt.subplots(nrows, ncols, **fig_kwargs)
-    for i, label in enumerate(labels):
-        pie_plot(df, label, ax=axes[i//nrows, i % nrows], show=False)
-
-    plt.suptitle(title or "", fontsize=16)
-
-    if show:
-        plt.show()
-
-    return fig
