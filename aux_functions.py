f5d1eb4e6443d224561f15dd0ebb8dee20b8124e
mysh <mysh995@gmail.com>
2019-11-27 15:32:34 +0000

    Bar plots with gaussian

--- aux_functions.py
+++ aux_functions.py
@@ -1,5 +1,8 @@
 
 def break_label(label, breakwords=None):
+    if not isinstance(label, str):
+        return label
+
     if breakwords is None:
         breakwords = ['and']
     for breakword in breakwords:
--- plotting_functions.py
+++ plotting_functions.py
@@ -1,7 +1,66 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from scipy.stats import norm
 
 import aux_functions as aux
+
+
+def count(df: pd.DataFrame, label):
+    return df[label].value_counts()
+
+
+def gauss(df: pd.DataFrame, label):
+    data = df[label]
+    mean = data.mean()
+    std = data.std()
+    return mean, std
+
+
+def pie_subplot(counts, ax, **kwargs):
+    pie = ax.pie(counts, **kwargs)
+
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    ax.legend(pie[0], aux.break_labels(counts.keys()), fontsize=10,
+              bbox_transform=ax.transAxes, bbox_to_anchor=(1, 0.5), loc="center left")
+
+
+def bar_subplot(counts, ax, gauss_params=None, bar_color='lightslategray', gauss_color='crimson', **kwargs):
+    keys = counts.keys()
+
+    ax.bar(keys, counts, color=bar_color, **kwargs, label='Counts')
+
+    if gauss_params is not None:
+        x = np.linspace(0, keys.max(), 100)
+        y = norm.pdf(x, *gauss_params)
+        y = y / y.max() * counts.max()
+        ax.plot(x, y, zorder=5, color=gauss_color, lw=3, label='Distribution')
+        ax.axvline(gauss_params[0], color=gauss_color, linestyle='--', label='Mean')
+
+    ax.legend(fancybox=True, framealpha=0.5)
+
+
+PLOTS_REGISTRY = {'pie': pie_subplot,
+                  'bar': bar_subplot}
+
+
+def make_subplot(df, label, type='pie', ax=None, show=True, break_labels=True, **kwargs):
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    counts = count(df, label)
+    if counts.keys().is_numeric():
+        kwargs['gauss_params'] = gauss(df, label)
+
+    PLOTS_REGISTRY[type](counts, ax, **kwargs)
+
+    if show:
+        plt.show()
+
+    ax.set_title(aux.break_label(counts.name, ['a ', 'do']) if break_labels else counts.name)
 
 
 def make_plot(df: pd.DataFrame, labels, nrows=2, show=True, title=None, fig_kwargs=None, **kwargs):
@@ -20,37 +79,3 @@ def make_plot(df: pd.DataFrame, labels, nrows=2, show=True, title=None, fig_kwar
         plt.show()
 
     return fig
-
-
-def count(df: pd.DataFrame, label):
-    return df[label].value_counts()
-
-
-def make_subplot(df, label, type='pie', ax=None, show=True, **kwargs):
-    if ax is None:
-        fig, ax = plt.subplots()
-
-    counts = count(df, label)
-
-    if type == 'pie':
-        pie_subplot(counts, ax, **kwargs)
-    else:
-        raise ValueError(f"Plot type '{type}' not recognised")
-
-    if show:
-        plt.show()
-
-    ax.set_title(aux.break_label(counts.name, ['a ', 'do']))
-
-
-def pie_subplot(counts, ax, **kwargs):
-    pie = ax.pie(counts, **kwargs)
-
-    # Shrink current axis by 20%
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-    ax.legend(pie[0], aux.break_labels(counts.keys()), fontsize=10,
-              bbox_transform=ax.transAxes, bbox_to_anchor=(1, 0.5), loc="center left")
-
-
