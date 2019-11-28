c0954f343ea554f34df66a52c3c80fc7608954d1
mysh <mysh995@gmail.com>
2019-11-28 19:40:11 +0000

    Comments analysis - more refactoring

--- aux_functions.py
+++ aux_functions.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import nltk
+from typing import Union
 
+import misc
 
 def break_label(label, breakwords=None):
     if not isinstance(label, str):
@@ -28,8 +31,34 @@ def prepare_data(fname=None):
     keys['tasks'] = k[10:18]
     keys['clustering'] = k[18:20]
     keys['yippy'] = k[20:23] + k[25:26]
-    keys['remarks_yippy'] = k[23:24]
-    keys['remarks_eval'] = [k[26], k[28]]
+    keys['remarks_yippy'] = k[23:25] + [k[26]]
+    keys['remarks_eval'] = [k[28]]
     keys['eval'] = [k[27]]
 
     return data, keys
+
+
+def count_words(data: Union[pd.Series, pd.DataFrame], label=None):
+    if isinstance(data, pd.DataFrame):
+        data = data[label]
+
+    merged_text = "".join(data.dropna()).lower()
+    words = nltk.tokenize.word_tokenize(merged_text)
+
+    stop_words = nltk.corpus.stopwords.words("english") + [',', '.', ';', ':', "'"]
+
+    ps = nltk.stem.PorterStemmer()
+
+    stemmed_words = misc.ListDict()
+
+    for w in words:
+        if w not in stop_words:
+            w_stem = ps.stem(w)
+            if w_stem not in stop_words:
+                stemmed_words[w_stem].append(w)
+
+    counts = {v[0]: len(v) for v in stemmed_words.values()}
+
+    counts_pandas = pd.Series(counts, name=label or "Word counts")
+
+    return counts_pandas
